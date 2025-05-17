9fe9cc5c86da956072f1291eca592a1c14ddaa3f
Harun Yahya Ünal <yahyaunal89@gmail.com>
2025-05-17 18:59:14 +0300

    models, 100 time each query

--- Term Project II/Advanced Database Topics II/SimulationPage.cs
+++ Term Project II/Advanced Database Topics II/SimulationPage.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace Advanced_Database_Topics_II
@@ -21,26 +15,59 @@ namespace Advanced_Database_Topics_II
 
         private async void btnSimulateQ1_Click(object sender, EventArgs e)
         {
-            var query1Top10 = await _simulation.GenerateQuery1();
-            dgwTop10.DataSource = query1Top10.Item1;
-            lblEffectedRow.Text = query1Top10.Item2.ToString();
-            lblDuration.Text = $"{query1Top10.Item3} ms";
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            for (int i = 0; i < 100; i++)
+            {
+                var query1Top10 = await _simulation.GenerateQuery1();
+                dgwTop10.DataSource = query1Top10.Item1;
+                lblEffectedRow.Text = query1Top10.Item2.ToString();
+                lblDuration.Text = $"{query1Top10.Item3} ms";
+                Console.WriteLine($"Query 1 - {i + 1} executed");
+            }
+            stopwatch.Stop();
+            var duration = stopwatch.ElapsedMilliseconds;
+            lblDuration.Text = $"Total: {duration} ms\nAverage: {duration/100} ms";
+            Console.WriteLine("**********Simulation Over********");
         }
 
         private async void btnSimulateQ2_Click(object sender, EventArgs e)
         {
-            var query2Top10 = await _simulation.GenerateQuery2();
-            dgwTop10.DataSource = query2Top10.Item1; 
-            lblEffectedRow.Text = query2Top10.Item2.ToString();
-            lblDuration.Text = $"{query2Top10.Item3} ms";
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            for (int i = 0; i < 100; i++)
+            {
+                var query2Top10 = await _simulation.GenerateQuery2();
+                dgwTop10.DataSource = query2Top10.Item1;
+                lblEffectedRow.Text = query2Top10.Item2.ToString();
+                lblDuration.Text = $"{query2Top10.Item3} ms";
+                Console.WriteLine($"Query 2 - {i + 1} executed");
+            }
+            stopwatch.Stop();
+            var duration = stopwatch.ElapsedMilliseconds;
+            lblDuration.Text = $"Total: {duration} ms\nAverage: {duration / 100} ms";
+            Console.WriteLine("**********Simulation Over********");
         }
 
         private async void btnSimulateQ3_Click(object sender, EventArgs e)
         {
-            var query3Top10 = await _simulation.GenerateQuery3();
-            dgwTop10.DataSource = query3Top10.Item1;
-            lblEffectedRow.Text = query3Top10.Item2.ToString();
-            lblDuration.Text = $"{query3Top10.Item3} ms";
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            for (int i = 0; i < 100; i++)
+            {
+                var query3Top10 = await _simulation.GenerateQuery3();
+                dgwTop10.DataSource = query3Top10.Item1;
+                lblEffectedRow.Text = query3Top10.Item2.ToString();
+                lblDuration.Text = $"{query3Top10.Item3} ms";
+                Console.WriteLine($"Query 3 - {i + 1} executed");
+            }
+            stopwatch.Stop();
+            var duration = stopwatch.ElapsedMilliseconds;
+            lblDuration.Text = $"Total: {duration} ms\nAverage: {duration / 100} ms";
+            Console.WriteLine("**********Simulation Over********");
         }
     }
 }
